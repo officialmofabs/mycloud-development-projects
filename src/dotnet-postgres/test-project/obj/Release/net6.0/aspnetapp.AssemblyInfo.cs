ce1ce6babc7cd41b6ae53b414c4862acc90ad65b
projectoperations <34292598+projectoperations@users.noreply.github.com>
2024-04-19 18:55:27 +0100

    main

--- src/dotnet-postgres/test-project/obj/Release/net6.0/aspnetapp.AssemblyInfo.cs
+++ src/dotnet-postgres/test-project/obj/Release/net6.0/aspnetapp.AssemblyInfo.cs
@@ -11,9 +11,8 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("31051026529000467138")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("aspnetapp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e9ad59c4a733d14f06829ee4c25e24b64009a55")]
 [assembly: System.Reflection.AssemblyProductAttribute("aspnetapp")]
